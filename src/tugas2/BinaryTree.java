34886fb46cd2f0fa517a3f7f6373d93941b4d935
RedHunter7 <adlirafif42121@gmail.com>
2022-11-23 14:19:56 +0700

    add odd even

--- src/tugas2/BinaryTree.java
+++ src/tugas2/BinaryTree.java
@@ -5,29 +5,43 @@ import tugas2.Node;
 public class BinaryTree {
 	private Node root;
 	
-	public BinaryTree(int value) {
-		root = new Node(value);
-	}
-	
 	public BinaryTree() {
 		root = null;
 	}
 	
 	public void insertTree(int value) {
 		Node newTree = new Node(value);
-		
 		if (root == null) {
-			root = newTree;
-			return;
+			root = new Node(0);
 		}
 		
 		Node current = root;
-		Node parent;
+		Node parent = current;
+		boolean isLeft = true;
+		
+		if (value % 2 == 0) {
+			current = current.leftChild;
+		} else {
+			isLeft = false;
+			current = current.rightChild;
+		}
+		
+		
+		if (current == null && isLeft) {
+			parent.leftChild = newTree;
+			return;
+		}
+		
+		if (current == null && !isLeft) {
+			parent.rightChild = newTree;
+			return;
+		}
+		
 		while (true) {
 			parent = current;
-			boolean isLeft = true;
 			
 			if (value < current.data) {
+				isLeft = true;
 				current = current.leftChild;
 			} else {
 				isLeft = false;
@@ -47,29 +61,76 @@ public class BinaryTree {
 	}
 	
 	public Node getMinimum() {
-		Node current = root;
-		
-		while (current.leftChild != null) {			
-			current = current.leftChild;
+		if (root == null) {
+			System.out.println("binary tree kosong");
+			return null;
 		}
 		
-		return current;
+		Node left = root.leftChild;
+		Node right = root.rightChild;
+		
+		if (left != null) {
+			while (left.leftChild != null) {			
+				left = left.leftChild;
+			}
+		}
+		
+		if (right != null) {
+			while (right.leftChild != null) {			
+				right = right.leftChild;
+			}
+		}
+		
+		Node min = null;
+		if (left != null && right != null) {
+			if (left.data < right.data) {
+				min = left;
+			} else min = right;
+		}
+		
+		if (left == null) min = right;
+		else if (right == null) min = left;
+		
+		return min;
 	}
 	
 	public Node find(int value) {
+		if (root == null) {
+			System.out.println("binary tree kosong");
+			return null;
+		}
+		
 		Node current = root;
+		// System.out.println(current.data);
+		if (value % 2 == 0) {
+			current = current.leftChild;
+			// System.out.println(current.data);
+		} else {
+			current = current.rightChild;
+		}
+		
+		if (current == null) {
+			System.out.println("tidak ditemukan");
+			return null;
+		}
+		
 		while(current.data != value) {
 			if (value < current.data) {
 				current = current.leftChild;
 			} else {
 				current = current.rightChild;
 			}
+			
+			if (current == null) {
+				System.out.println("tidak ditemukan");
+				break;
+			}
 		}
 		
 		return current;
 	}
 	
-	public void delete(int value) {
+	public void delete() {
 		root = null;
 	}
 }
--- src/tugas2/Main.java
+++ src/tugas2/Main.java
@@ -3,12 +3,14 @@ import tugas2.BinaryTree;
 
 public class Main {
 	public static void main(String[] args) {
-        BinaryTree x = new BinaryTree(20);
-        // x.insertTree(30);
+        BinaryTree x = new BinaryTree();
+        x.insertTree(30);
         x.insertTree(2);
+        x.insertTree(4);
         x.insertTree(1);
-        x.delete(1);
+        x.insertTree(3);
+        // x.delete();
         System.out.println(x.getMinimum().data);
-        System.out.println(x.find(20).data);
+        System.out.println(x.find(3).data);
     }
 }
